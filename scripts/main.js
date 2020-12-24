f121f16000774216a0bb75759d29ef0d52e1e969
bobanderic2000 <bobanderic2000@gmail.com>
2020-12-24 16:10:32 +0800

    購物車總價、小計完成 (2hr10min)

--- scripts/main.js
+++ scripts/main.js
@@ -5,23 +5,45 @@ document.addEventListener('DOMContentLoaded', () => {
   })
   // 刪除項目
   function deleteItem(e) {
-    let cartItem = e.currentTarget.parentElement.parentElement
+    const cartItem = e.currentTarget.parentElement.parentElement
     cartItem.remove()
     updateTotal()
   }
   
-  function updateTotal(){
-    let total = document.querySelector('.total').textContent.replace('$', '');
-    let cartItems = document.querySelectorAll('.cart-item');
-    cartItems.forEach(cartItem => {
-      let price = cartItem.querySelector('.cart-item-price').textContent.replace('$', '')
-      let quantity = cartItem.querySelector('.quantity').value
-      console.log(price);
-      // console.log(cartItems);
-    })
+  // 購物車品項的數量變化時，小計金額變化
+  const inputs = document.querySelectorAll('.quantity');
+  inputs.forEach(input => {
+    input.addEventListener('change', setQuantity)
+  })
+
+
+  // 單品數量
+  function setQuantity(e) {
+    const input = e.target
+    let quantity = input.value;
+    // 設定數量不可小於1
+    if (quantity <= 1) {
+      quantity = 1
+      e.currentTarget.value = quantity
+    }
+    // 小計計算
+    let itemPrice = input.parentNode.parentNode.querySelector('.cart-item-price').textContent.replace('$', '');
+    let subtotalCount = (quantity * itemPrice);
+    input.parentNode.parentNode.querySelector('.subtotal').textContent = `$${subtotalCount}`
+    updateTotal();
   }
 
-  
+  // 計算總價
+  function updateTotal(){
+    let total = 0;
+    const cartItems = document.querySelectorAll('.cart-item');
+    cartItems.forEach(cartItem => {
+      let price = cartItem.querySelector('.cart-item-price').textContent.replace('$', '');
+      let quantity = cartItem.querySelector('.quantity').value;
+      total += (price * quantity);  // 這行比較特別，容易忘，可以記一下
+    });
+    document.querySelector('.total').textContent = `$${total}`
+  } 
 
 // this is the end  
 })
